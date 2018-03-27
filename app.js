368c43379b069065072b74b780606423554d4b51
Kaden Zipfel <h.fan98@hotmail.com>
2018-03-27 01:59:30 +0000

    Add index route

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+app.listen(process.env.PORT, () => {
+  console.log('Server is running');
+});
